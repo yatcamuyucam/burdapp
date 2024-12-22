a07d970a6cb2e1f6fe8f80c482027c4019ad3f03
yatcamuyucam <yusufatas2002@gmail.com>
2024-12-22 20:51:59 +0300

    ticket frontend done

---
+++ app/src/main/java/com/example/burdapp/Activity/TicketActivity.java
@@ -0,0 +1,22 @@
+package com.example.burdapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.burdapp.R;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ticket);
+
+    }
+}
